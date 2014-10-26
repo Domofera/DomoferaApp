3d439ff084025333796be12ec0de0e07269d71ab
Elena Torro <elenatorro@gmail.com>
2014-10-26 18:16:42 +0100

    some crud operations added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
+
 	def index
 	end
 
+	def new
+		@user = User.new
+	end
+
 	def create
+		@user = User.new(params[:user])
+		if @user.save
+			render "index", status: 201
+		else
+			render "new"
+		end
 	end
 
 	def delete
