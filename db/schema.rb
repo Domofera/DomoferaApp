51c30fc2b96c1549cb0e8c21b80bc25100bf359c
Elena Torro <elenatorro@gmail.com>
2014-10-26 16:18:09 +0100

    added password to user in db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021150007) do
+ActiveRecord::Schema.define(version: 20141026151424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20141021150007) do
     t.string   "city"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_hash"
+    t.string   "password_salt"
   end
 
 end
