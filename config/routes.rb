ff781d53e157aee2f5bb6d9d969c8704959a72a7
Elena Torro <elenatorro@gmail.com>
2014-10-26 18:18:16 +0100

    routes for users added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+ get 'sessions/new'
+ get "log_out" => "sessions#destroy", :as => "log_out"
+ get "log_in" => "sessions#new", :as => "log_in"
+ get "sign_up" => "users#new", :as => "sign_up"
+
  root 'home#index'
- resources :users, only: [:index, :new]
+ resources :users, only: [:index, :new, :create, :delete]
+ resources :user, only: [:show]
+ resources :sessions, only: [:new, :create, :destroy]
 end
