b638824e30cab719b3f90e4911f638d02867b9d6
Elena Torro <elenatorro@gmail.com>
2014-10-26 18:15:52 +0100

    resources and flash in angular added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require angular/angular
 //= require angular-route/angular-route
+//= require angular-resource/angular-resource
+//= require angular-flash/dist/angular-flash
 //= require angular-rails-templates
 //= require_tree .
 
