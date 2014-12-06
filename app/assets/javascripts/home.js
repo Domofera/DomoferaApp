9293ca4fb347aa257575d3cd9766e525d7cf9b7a
Elena Torro <elenatorro@gmail.com>
2014-12-06 16:17:58 +0100

    changing js assets

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var po = document.createElement('script');
+po.type = 'text/javascript'; po.async = true;
+po.src = 'https://plus.google.com/js/client:plusone.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(po, s);
