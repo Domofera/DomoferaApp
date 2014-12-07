5b4303dcbb1a4dce326d70b84b508562f1067740
Elena Torro <elenatorro@gmail.com>
2014-12-07 17:32:17 +0100

    renamed sensors to 'name' and translated to spanish

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -5,6 +5,15 @@ class SensorsController < ApplicationController
     @sensor = Sensor.new
   end
 
+  def create
+    @sensor = Sensor.new sensor_params
+    if @sensor.save
+      flash[:notice] = "Sensor creado"
+    else
+      flash[:notice] = @sensor.errors.full_messages.to_a.join(", ")
+    end
+  end
+
   def edit
     @sensor = get_sensor
   end
@@ -17,7 +26,7 @@ class SensorsController < ApplicationController
 
   private
   def sensor_params
-    params.require(:sensor).permit(:value, :terminal_id, :id)
+    params.require(:sensor).permit(:value, :terminal_id, :id, :name)
   end
 
   def get_sensor
