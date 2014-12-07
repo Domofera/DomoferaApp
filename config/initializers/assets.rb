72153f6f0a7c8399b627b6cb75ee1883efeaf7f0
Elena Torro <elenatorro@gmail.com>
2014-12-07 23:15:02 +0100

    assets fixed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,9 +2,5 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( script.js )
-Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( compiled/script.js )
+Rails.application.config.assets.precompile += %w( compiled/style.css )
