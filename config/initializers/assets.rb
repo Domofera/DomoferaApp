43c9735b2526bb2256cda15a78906a79e60a4f8a
Elena Torro <elenatorro@gmail.com>
2014-12-09 09:52:23 +0100

    added precompiled statistics

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,5 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( compiled/script.js )
+Rails.application.config.assets.precompile += %w( compiled/statistics.js )
 Rails.application.config.assets.precompile += %w( compiled/style.css )
