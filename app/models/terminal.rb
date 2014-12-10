a29d283c07457f4a64aedb02b3a84d7932c3af0f
Elena Torro <elenatorro@gmail.com>
2014-12-10 14:03:44 +0100

    using custom messages

--- app/models/terminal.rb
+++ app/models/terminal.rb
@@ -2,9 +2,13 @@ class Terminal < ActiveRecord::Base
 	belongs_to :user
 	has_many   :sensors, :dependent => :destroy
 
-	validates :name, presence: true,
-					 length: { maximum: 20 },
-					 format: { with: /\A[a-zA-Z0-9' ']*\z/ }
+	validates :name, :presence => { :message => "El nombre no puede estar vacío" }
+	validates :name, :length   => { :maximum => 20,
+																	:message => "El nombre no puede tener más de
+																							20 caracteres"}
+  validates :name, :format   => { :with    => /\A[a-zA-Z0-9' ']*\z/,
+																	:message => "El nombre solo puede contener caracteres,
+																							numeros y espacios en blanco"}
 
   validates :password, presence: true,
            length: { maximum: 20 },
@@ -13,4 +17,5 @@ class Terminal < ActiveRecord::Base
 	validates :description,
 					 length: { maximum: 60 },
 					 format: { with: /\A[a-zA-Z0-9' ']*\z/ }
+
 end
