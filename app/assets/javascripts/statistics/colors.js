31bdf002309f743b52d6da51d0cd081e24d76f62
Elena Torro <elenatorro@gmail.com>
2014-12-12 22:35:04 +0100

    graphs changed, improved colors and buttons

--- app/assets/javascripts/statistics/colors.js
+++ app/assets/javascripts/statistics/colors.js
@@ -3,23 +3,23 @@ var Color = function() {
     'blue'          :'#0CB6B6',
     'orange'        :'#FF6600',
 
-    'humidityLight' :'#81CFE0',
-    'humidityDark'  :'#52B3D9',
+    'humidityLight' :'#47A3FF',
+    'humidityDark'  :'#007AA3',
 
-    'windLight'     :'#E6B8E6',
-    'windDark'      :'#8A6E8A',
+    'windLight'     :'#E62E8A',
+    'windDark'      :'#B2246B',
 
-    'floorLight'    :'#D65C33',
+    'floorLight'    :'#CC3300',
     'floorDark'     :'#8F2400',
 
-    'tempLight'     :'#FF9933',
-    'tempDark'      :'#E68A2E',
+    'tempLight'     :'#FF9900',
+    'tempDark'      :'#CC7A00',
 
     'lightLight'    :'#FFFF99',
     'lightDark'     :'#FFCC00',
 
-    'phLight'       :'#00FF99',
-    'phDark'        :'#00CC7A'
+    'phLight'       :'#009933',
+    'phDark'        :'#006B24'
   }
 }
 
