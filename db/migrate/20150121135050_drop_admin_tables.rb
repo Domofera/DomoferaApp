e911290a86d774bd5b6f528af30e7603f51b2d6a
Elena Torro <elenatorro@gmail.com>
2015-02-11 16:53:11 +0100

    solving error in deployment

--- db/migrate/20150121135050_drop_admin_tables.rb
+++ db/migrate/20150121135050_drop_admin_tables.rb
@@ -1,6 +1,5 @@
 class DropAdminTables < ActiveRecord::Migration
   def change
-    drop_table :active_admin_comments
     drop_table :admin_users
   end
 end
---
+++ spec/controllers/month_records_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe MonthRecordsController, :type => :controller do
+
+end
