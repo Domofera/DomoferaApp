c59a4adcaf83c8a4cf876ab613a60b193d0ced8f
Elena Torro <elenatorro@gmail.com>
2015-03-07 08:05:17 +0100

    added fullcalendar to config assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,4 +4,6 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( compiled/script.js )
 Rails.application.config.assets.precompile += %w( compiled/statistics.js )
+Rails.application.config.assets.precompile += %w( compiled/calendar.js )
 Rails.application.config.assets.precompile += %w( compiled/style.css )
+Rails.application.config.assets.precompile += %w( compiled/fullcalendar.css )
