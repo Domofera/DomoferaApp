7fd22ed840bec15387441aa5fbecc80d3f3cf022
Elena Torro <elenatorro@gmail.com>
2015-03-15 11:51:42 +0100

    improved statistics view

--- app/assets/javascripts/statistics/windowLoad.js
+++ app/assets/javascripts/statistics/windowLoad.js
@@ -1,21 +1,109 @@
 window.onload = function(){
-  var username = $('#username').text();
-  var month    = $('#monthParam').val();
-  var year     = $('#yearParam').val();
-  var day      = $('#dayParam').val();
-  var mode     = $('#modeParam').val();
+  var today = new Date();
+  $('#monthParam').val(today.getMonth()+1);
+  $('#yearParam').val(today.getFullYear());
+  $('#dayParam').val(today.getDate());
+  loadData();
+};
 
+var loadData = function() {
+  var equivalence = {
+    "Día" : "d",
+    "Mes" : "m",
+    "Año" : "y",
+    "Temperatura del aire" : "temperature_air",
+    "Temperatura del suelo": "temperature_floor",
+    "Humedad del aire"     : "humidity_air",
+    "Humedad del suelo"    : "humidity_floor",
+    "Luz"                  : "light"
+  };
+
+  var username  = $('#username').text();
+  var month     = $('#monthParam').val();
+  var year      = $('#yearParam').val();
+  var day       = $('#dayParam').val();
+  var mode      = equivalence[$('#modeParam').val()];
+  var attribute = equivalence[$('#attributeParam').val()];
+  console.log(attribute);
 
   $('.canvasContainer').each(function(index) {
     var canvasContext = this.firstChild.nextSibling.getContext("2d");
-    var data = getTerminalData(username, this.id, day, month, year, mode, canvasContext);
-
+    var data = getTerminalData(username, this.id, day, month, year, mode, canvasContext, attribute);
   })
 };
 
+var Colors = {
+  temperature_floor: {
+    min: [102,61,0],
+    max: [178, 107,71],
+    average: [230, 138, 92]
+  },
+  temperature_air: {
+    min: [255,153,102],
+    max: [129, 161,97],
+    average: [184, 230, 138]
+  },
+  humidity_floor: {
+    min: [173,133,255],
+    max: [110, 85, 163],
+    average: [138, 106, 204]
+  },
+  humidity_air: {
+    min: [0,153,255],
+    max: [0, 85,143],
+    average: [0, 122, 204]
+  },
+  light: {
+    min: [178, 71, 0],
+    max: [255,204, 0],
+    average: [255, 224, 102]
+  }
+}
+
+var chart = null;
+
+document.getElementById('update-data').addEventListener('click', function() {
+  loadData();
+});
+
+function drawMonth(days, value) {
+  var max = value + '_max';
+  var min = value + '_min';
+  var average = value + '_average';
+  var colors = Colors[value];
+  var data = {
+      labels: getDaysArray(days),
+      datasets: [
+          {
+              label: "Mínimo",
+              fillColor: "rgba("+ colors.min[0] +"," + colors.min[1] +"," + colors.min[2] +",0.5)",
+              strokeColor: "rgba("+ colors.min[0] +"," + colors.min[1] +"," + colors.min[2] +",0.5)",
+              highlightFill: "rgba("+ colors.min[0] +"," + colors.min[1] +"," + colors.min[2] +",1)",
+              highlightStroke: "rgba("+ colors.min[0] +"," + colors.min[1] +"," + colors.min[2] +",1)",
+              data: getDaysArrayData(days, min)
+          },
+          {
+              label: "Medio",
+              fillColor: "rgba("+ colors.average[0] +"," + colors.average[1] +"," + colors.average[2] +",0.5)",
+              strokeColor: "rgba("+ colors.average[0] +"," + colors.average[1] +"," + colors.average[2] +",0.5)",
+              highlightFill: "rgba("+ colors.average[0] +"," + colors.average[1] +"," + colors.average[2] +",1)",
+              highlightStroke: "rgba("+ colors.average[0] +"," + colors.average[1] +"," + colors.average[2] +",1)",
+              data: getDaysArrayData(days, average)
+          },
+          {
+              label: "Máximo",
+              fillColor: "rgba("+ colors.max[0] +"," + colors.max[1] +"," + colors.max[2] +",0.5)",
+              strokeColor: "rgba("+ colors.max[0] +"," + colors.max[1] +"," + colors.max[2] +",0.5)",
+              highlightFill: "rgba("+ colors.max[0] +"," + colors.max[1] +"," + colors.max[2] +",1)",
+              highlightStroke: "rgba("+ colors.max[0] +"," + colors.max[1] +"," + colors.max[2] +",1)",
+              data: getDaysArrayData(days, max)
+          }
+      ]
+  };
+  return data;
+}
 
 function drawDay(dayData) {
-  console.log(dayData);
   var data = {
       labels: ["Temp Aire", "Temp Suelo", "Hum Aire",
               "Hum Suelo",   "Luz"],
@@ -55,11 +143,73 @@ function drawDay(dayData) {
   return data;
 };
 
+function drawYear(data, value, colors) {
+  var max = value + '_max';
+  var min = value + '_min';
+  var average = value + '_average';
+  var colors = Colors[value];
+  var data = {
+      labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+               "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+      datasets: [
+          {
+              label: "Mínimo",
+              fillColor: "rgba("+ colors.min[0] +"," + colors.min[1] +"," + colors.min[2] +",0.5)",
+              strokeColor: "rgba("+ colors.min[0] +"," + colors.min[1] +"," + colors.min[2] +",0.5)",
+              highlightFill: "rgba("+ colors.min[0] +"," + colors.min[1] +"," + colors.min[2] +",1)",
+              highlightStroke: "rgba("+ colors.min[0] +"," + colors.min[1] +"," + colors.min[2] +",1)",
+              data: getYearArrayData(data, min)
+          },
+          {
+              label: "Medio",
+              fillColor: "rgba("+ colors.average[0] +"," + colors.average[1] +"," + colors.average[2] +",0.5)",
+              strokeColor: "rgba("+ colors.average[0] +"," + colors.average[1] +"," + colors.average[2] +",0.5)",
+              highlightFill: "rgba("+ colors.average[0] +"," + colors.average[1] +"," + colors.average[2] +",1)",
+              highlightStroke: "rgba("+ colors.average[0] +"," + colors.average[1] +"," + colors.average[2] +",1)",
+              data: getYearArrayData(data, average)
+          },
+          {
+              label: "Máximo",
+              fillColor: "rgba("+ colors.max[0] +"," + colors.max[1] +"," + colors.max[2] +",0.5)",
+              strokeColor: "rgba("+ colors.max[0] +"," + colors.max[1] +"," + colors.max[2] +",0.5)",
+              highlightFill: "rgba("+ colors.max[0] +"," + colors.max[1] +"," + colors.max[2] +",1)",
+              highlightStroke: "rgba("+ colors.max[0] +"," + colors.max[1] +"," + colors.max[2] +",1)",
+              data: getYearArrayData(data, max)
+          }
+      ]
+  };
+  return data;
+}
+
 function drawChart(data, mode) {
   return drawDay(data);
 };
 
-function getTerminalData(username, id, day, month, year, mode, context) {
+function getDaysArray(days) {
+  var arr = [];
+  days.forEach(function(element, index) {
+    arr.push(index+1);
+  })
+  return arr;
+};
+
+function getDaysArrayData(days, value) {
+  var arr = [];
+  days.forEach(function(element) {
+    arr.push(element[value])
+  });
+  return arr;
+};
+
+function getYearArrayData(data, value) {
+  var arr = [];
+  data.forEach(function(element) {
+    arr.push(element[value]);
+  })
+  return arr;
+};
+
+function getTerminalData(username, id, day, month, year, mode, context, attribute) {
   var url  = "http://localhost:3000/api/statistics?username="+ username
                                                  + "&id="    + id
                                                  + "&day="   + day
@@ -72,13 +222,17 @@ function getTerminalData(username, id, day, month, year, mode, context) {
     success: function(data) {
       if (mode == 'd') {
         var dayData = drawDay(data);
-        var newChart = new Chart(context).Bar(dayData);
+        if (chart) chart.destroy();
+        chart = new Chart(context).Bar(dayData);
       } else if (mode == 'm') {
-        var monthData = drawMonth(data);
-        var newChart = new Chart(context).Line(monthData);
+
+        var month  = drawMonth(data, attribute);
+        if (chart) chart.destroy();
+        chart = new Chart(context).Line(month);
       } else if (mode == 'y') {
-        var yearData = drawYear(data);
-        var newChart = new Chart(context).Line(yearData);
+        var yearData = drawYear(data, attribute);
+        if (chart) chart.destroy();
+        chart = new Chart(context).Line(yearData);
       }
     }
   });
